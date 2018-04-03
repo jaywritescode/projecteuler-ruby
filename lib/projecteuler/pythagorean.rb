5b41bdede70d3aed90b7dd75cf73c6ce6eee99c5
Jay Harris <jaywritescode@users.noreply.github.com>
2018-04-03 19:19:52 -0400

    consolidate primitive pythagorean tree methods, add places to stop

--- lib/projecteuler/pythagorean.rb
+++ lib/projecteuler/pythagorean.rb
@@ -1,42 +1,33 @@
+require 'bigdecimal'
 require 'prime'
-require 'pry-byebug'
 
 module Pythagorean
+
+  Matrices1 = [[[1,-2,2],[2,-1,2],[2,-2,3]],
+                [[1,2,2],[2,1,2],[2,2,3]],
+                [[-1,2,2],[-2,1,2],[-2,2,3]]]
+  Matrices2 = [[[2,1,-1],[-2,2,2],[-2,1,3]],
+                [[2,1,1],[2,-2,2],[2,-1,3]],
+                [[2,-1,2],[2,2,2],[2,1,3]]]
+
   # returns an enumerator over all primitive pythagorean triples
-  def primitive_tree
-    m1 = [[1,-2,2],[2,-1,2],[2,-2,3]]
-    m2 = [[1,2,2],[2,1,2],[2,2,3]]
-    m3 = [[-1,2,2],[-2,1,2],[-2,2,3]]
-
+  def primitive_tree(matrices: Matrices1,
+                     short_leg_max_length: BigDecimal('Infinity'),
+                     long_leg_max_length: BigDecimal('Infinity'))
     Enumerator.new do |y|
       queue = [[3, 4, 5]]
 
       loop do
+        break if queue.empty?
+
         triple = queue.shift
 
         y << triple
-        [m1, m2, m3].each do |m|
-          queue << m.map {|row| dot_product(row, triple)}
-        end
-      end
-    end
-  end
+        matrices.each do |matrix|
+          next_triple = matrix.map { |row| dot_product(row, triple) }.sort
 
-  # an alternate enumerator over all primitive pythagorean triples
-  def primitive_tree2
-    m1 = [[2,1,-1],[-2,2,2],[-2,1,3]]
-    m2 = [[2,1,1],[2,-2,2],[2,-1,3]]
-    m3 = [[2,-1,2],[2,2,2],[2,1,3]]
-
-    Enumerator.new do |y|
-      queue = [[3, 4, 5]]
-
-      loop do
-        triple = queue.shift
-
-        y << triple
-        [m1, m2, m3].each do |m|
-          queue << m.map {|row| dot_product(row, triple)}
+          queue << next_triple unless next_triple[0] > short_leg_max_length ||
+                                      next_triple[1] > long_leg_max_length
         end
       end
     end
